21b118dc71c91830cf9b27890d3298605ce4abab
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2017-02-25 14:48:40 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+brew install fish
+brew install node
+
+npm install -g http-server
+npm install -g tern
---
+++ omf/init.fish
@@ -0,0 +1,6 @@
+set fish_greeting 'hello gunderwonder'
+
+set DOTFILE_BASE_PATH (dirname (status -f))/../
+
+source $DOTFILE_BASE_PATH/aliases.fish
+source $DOTFILE_BASE_PATH/exports.fish
