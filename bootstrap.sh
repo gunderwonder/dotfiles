eaebb70e379603ff4145cc624b9205a1b7401413
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2018-04-02 22:13:07 +0200

    Miscellaneous new stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
 brew install fish
+
+brew tap caskroom/cask
+
+brew cask install sourcetree
+brew cask install anaconda
+brew cask install google-chrome
+brew cask install docker
+brew cask install iterm2
+
 brew install node
 
 npm install -g http-server
+npm install -g eslint
 npm install -g tern
