1d2342df49997ede5080f2af96138d08339d460d
Øystein Riiser Gundersen <gunderwonder@zeus.local>
2018-04-16 22:29:39 +0200

    Misc. apps + Atom/Hydrogen.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,31 @@
 brew install fish
-
-brew tap caskroom/cask
-
-brew cask install sourcetree
-brew cask install anaconda
-brew cask install google-chrome
-brew cask install docker
-brew cask install iterm2
+curl -L https://get.oh-my.fish > install
+fish install --path=~/.local/share/omf --config=~/.dotfiles/omf/
 
 brew install node
 
 npm install -g http-server
 npm install -g eslint
-npm install -g tern
+
+brew tap caskroom/cask
+
+brew cask install sourcetree
+brew cask install controllermate
+brew cask install devonthink-pro
+brew cask install google-chrome
+brew cask install docker
+brew cask install iterm2
+brew cask install spotify
+brew cask install 1password
+brew cask install dropbox
+brew cask install telegram-desktop
+brew cask install istat-menus
+
+brew cask install atom
+apm install sync-settings
+
+brew install python3
+brew install zeromq
+sudo python3 -m pip install jupyter
+npm install -g ijavascript
+ijsinstall
