db819986a2a8695642bdec1a9cd51899d38a2e18
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2022-01-08 18:51:14 +0100

    Oppdatering for 2022 (procida)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,56 +1,69 @@
 #!/usr/bin/env bash
 
+# konfigurasjon
 ln -s $(pwd)/.gitconfig ~/
 ln -s $(pwd)/.editorconfig ~/
 ln -s $(pwd)/ssh/config ~/.ssh/config
 ln -s $(pwd)/jupyter ~/.jupyter
 
+# aliaser
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Filer ~/
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Papirløst ~/
+ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Øystein ~/
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Assets ~/
+ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Prosjekter ~/
 
-brew install hub fish
-curl -L https://get.oh-my.fish > fish_install
-fish fish_install --path=~/.local/share/omf --config=$(pwd)/omf/
-rm install
-
-brew install mas
-brew install jq
-
+# utviklerverktøy
 brew install node
 npm install -g http-server \
 	 eslint \
 	 yarn
-
-brew tap caskroom/cask
-
-brew cask install iterm2 \
-	controllermate \
-	hazel \
-	devonthink \
-	soundsource \
-	google-chrome \
-	google-backup-and-sync \
-	istat-menus \
-	spotify \
-	1password \
-	dropbox \
-	aerial \
-	keyboard-maestro \
-	transmit \
-	dash \
-	paw \
-	tad \
-	sourcetree
-
-# https://github.com/sindresorhus/quick-look-pluginsx
-# brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize
-
-brew cask install atom
-apm install sync-settings
+brew install jq
 
 brew install python3
 brew install jupyter
+
+brew install visual-studio-code
+# sync: github
+# font: Jetbrains Mono (~/Assets)
+
+brew install dash
+
+# fish
+brew install hub fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
+fish install --path=~/.local/share/omf --config=$(pwd)/omf/
+rm install
+
+brew  install iterm2
+# sync: dropbox (~/Dropbox/Apps/iTerm)
+# font: Jetbrains Mono for Powerline (~/Assets)
+
+# verktøy
+brew install alfred
+# sync: dropbox (~/Dropbox/Konfigurasjon)
+
+brew install bartender
+brew install paletro
+brew install hook # sync: icloud
+brew install meastral # dropbox
+brew install istat-menus
+brew install karabiner-elements
+brew install hazel # sync: dropbox
+brew install google-drive-file-stream
+brew install homebrew/cask/onedrive
+
+# apps
+brew install aerial
+brew install spotify
+brew install devonthink
+brew install soundsource
+brew install 1password # sync: 1password
+brew install soundsource
+brew install raindropio
+
+# https://github.com/sindresorhus/quick-look-pluginsx
+# brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize
 
 pip3 install elasticsearch \
 	pandas \
--- exports.fish
+++ exports.fish
@@ -1,2 +1,1 @@
-set -g -x KULTURUKE_CREDENTIALS_PATH "$HOME/Dropbox/Apps/Google Drive/nrk-kulturuke-credentials.json"
 set -g -x NLTK_DATA "$HOME/.local/share/nltk_data"
